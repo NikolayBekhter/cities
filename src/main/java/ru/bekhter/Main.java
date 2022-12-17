473fa2a00369751a83d39789f7b948f0772ae792
BekhterNikolay <n.v.bekhter@mail.ru>
2022-12-17 21:01:40 +0300

    task 1.1

--- src/main/java/ru/bekhter/Main.java
+++ src/main/java/ru/bekhter/Main.java
@@ -1,7 +1,34 @@
 package ru.bekhter;
 
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+        String path = "C:/Users/nvbek/IdeaProjects/cities/city_ru.csv";
+        Scanner scanner = new Scanner(Paths.get(path));
+        scanner.useDelimiter(System.getProperty("line.separator"));
+        while(scanner.hasNext()){
+            //парсим строку в объект Employee
+            City city = parseCSVLine(scanner.next());
+            System.out.println(city.toString());
+        }
+        scanner.close();
+    }
+
+    private static City parseCSVLine(String line) {
+        Scanner scanner = new Scanner(line);
+        scanner.useDelimiter(";");
+        int id = scanner.nextInt();
+        String name = scanner.next();
+        String region = scanner.next();
+        String district = scanner.next();
+        int population = scanner.nextInt();
+        String foundation = null;
+        if (scanner.hasNext()) {
+            foundation = scanner.next();
+        }
+        return new City(name, region, district, population, foundation);
     }
 }
